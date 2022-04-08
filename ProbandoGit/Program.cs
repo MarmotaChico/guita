90cb61e89a350fdc7390adc4e84d5dffd0ca57df
Marmotachico <marmotachico@gmail.com>
2022-04-07 21:16:07 -0300

    Primer commit

---
+++ ProbandoGit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProbandoGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
